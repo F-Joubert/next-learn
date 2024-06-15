866be054482b8f2d8abc3b56c7fe4089b44e5cbe
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-06-15 14:30:02 +1000

    Created layout.tsx
    Created layout for new dashboards, updated SideNav component to use new logo

--- dashboard/app/ui/dashboard/sidenav.tsx
+++ dashboard/app/ui/dashboard/sidenav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
+import NandroLogo from '@/app/ui/nandro-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
@@ -11,7 +11,7 @@ export default function SideNav() {
         href="/"
       >
         <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
+          <NandroLogo />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
